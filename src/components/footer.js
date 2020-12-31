0a911591fb502d47eb2d0a57b328297a43ae6d7c
Ryan Miller <ryan@intranoggin.com>
2020-12-31 15:17:42 -0500

    After 18 month break, restarting from scratch.

--- src/components/footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from '../styles/footer.module.scss'
-
-export default()=>(
-<footer className={styles.footer}>	
-footer
-</footer>
-)
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import styles from '../styles/layout.module.scss'
-import Header from './header.js'
-import Footer from './footer.js'
-export default({children})=>(
-    <div>
-        <Header/>
-        <div section="main" className={styles.main}>
-            <div className={styles.leftChannel}>left</div>
-            <div className={styles.content}>{children}</div>    
-        </div>
-        <Footer/>
+import React from "react"
+import { Link } from "gatsby"
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
+  let header
+
+  if (isRootPath) {
+    header = (
+      <h1 className="main-heading">
+        <Link to="/">{title}</Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <Link className="header-link-home" to="/">
+        {title}
+      </Link>
+    )
+  }
+
+  return (
+    <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <header className="global-header">{header}</header>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
     </div>
-)
+  )
+}
+
+export default Layout
